e582f20d1531e2fce1514135f3891c34e7fc7f0f
w3tra <w3tra@mail.ru>
2017-03-15 12:36:20 +0600

    activeadmin installed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  def authenticate_active_admin_user!
+   authenticate_user!
+   unless current_user.role?(:admin)
+      flash[:alert] = "You are not authorized to access this resource!"
+      redirect_to root_path
+   end
+	end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  
+  def role?(r)
+  	role.include? r.to_s
+	end
+	
+end
